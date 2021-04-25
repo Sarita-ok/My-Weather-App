dd4f23b048d6da0eeb221293d1c89979b13061be
Sarita-ok <sarakhan@windowslive.com>
2021-04-25 12:35:11 +0100

    index.html file is added

---
+++ style.cc
@@ -0,0 +1,54 @@
+body {
+  font-family: arial, sans-serif;
+  margin: 20px auto;
+}
+header {
+  margin: auto;
+}
+.weather.container {
+  margin: 10px auto;
+  border: 3px solid #0f4c75;
+  border-radius: 50px;
+  padding: 20px;
+  background-color: #f0f5f9;
+  box-shadow: 3px 3px 3px #52616b;
+  max-width: 35%;
+}
+h1,
+h2 {
+  color: #0f4c75;
+  font-weight: 900;
+  font-size: 34px;
+  line-height: 35px;
+  padding: 0 5px;
+  text-align: center;
+  line-height: 35px;
+  margin: 5px auto;
+  display: block;
+}
+
+.weather-view__button--temp {
+  border-radius: 2px 0 0 2px;
+  border-right: none;
+}
+
+.weather-view__button--wind {
+  border-left: none;
+  border-radius: 0 2px 2px 0;
+}
+
+li {
+  list-style: none;
+}
+li.current-forecast {
+  font-weight: 600;
+  font-size: 25px;
+}
+
+li.current-date {
+  font-weight: 450;
+  font-size: 18px;
+}
+.img {
+  margin: auto;
+}
